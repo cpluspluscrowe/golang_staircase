49cd2d888c01d94f102292723ae345269d0a62c1
Chad Crowe <ccrowe@linkedin.com>
2018-06-09 17:17:42 -0500

    Added profiling

--- staircase.go
+++ staircase.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"github.com/pkg/profile"
+	"os"
+	"strconv"
 )
 
 func main() {
-	fmt.Printf("Hello")
+	defer profile.Start().Stop()
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+	var input, _, _ = reader.ReadLine()
+	var height, _ = strconv.Atoi(string(input))
+	var level int = 0
+	for i := 0; i < height; i++ {
+		var buffer bytes.Buffer
+		for j := 0; j < height-level; j++ {
+			buffer.WriteString(" ")
+		}
+		for j := 0; j < level; j++ {
+			buffer.WriteString("#")
+		}
+		level += 1
+		fmt.Println(buffer.String())
+	}
 }
